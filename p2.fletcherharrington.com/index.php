9e0e3a7e51e8d032780ab380cf88645e4045b1a8
Fletcher <fboost@gmail.com>
2012-10-17 12:08:15 -0400

    testing ignore

--- p2.fletcherharrington.com/index.php
+++ p2.fletcherharrington.com/index.php
@@ -34,47 +34,49 @@
 	
 ?>
 
-<?php  
-class MyClass  
-{  
-	public $prop1 = "I'm a class property!";  
-	public $prop2 = "I'm a class property 2";
-	
-	public function __construct() 
-	{
-		echo 'The class "', __CLASS__, '" was initiated!<br />';
-	}
-	public function __destruct() 
-    { 
-        echo 'The class "', __CLASS__, '" was destroyed.<br />';  
-    }  	
-    public function __toString()  
+    <?php  
+    class MyClass  
     {  
-        echo "Using the toString method: ";  
-        return $this->getProperty();  
-    }  	
-	public function setProperty($newval)  
-    {  
-        $this->prop1 = $newval;  
+        public $prop1 = "I'm a class property!";  
+        public function __construct()  
+        {  
+            echo 'The class "', __CLASS__, '" was initiated!<br />'; 
+        } 
+        public function __destruct() 
+        { 
+            echo 'The class "', __CLASS__, '" was destroyed.<br />';  
+        }  
+        public function __toString()  
+        {  
+            echo "Using the toString method: ";  
+            return $this->getProperty();  
+        }  
+        public function setProperty($newval)  
+        {  
+            $this->prop1 = $newval;  
+        }  
+        public function getProperty()  
+        {  
+            return $this->prop1 . "<br />";  
+        }  
     }  
-    public function getProperty()  
+    class MyOtherClass extends MyClass  
     {  
-        return $this->prop1 . "<br />";
+        public function __construct()  
+        {  
+            echo "A new constructor in " . __CLASS__ . ".<br />";  
+        }  
+        public function newMethod()  
+        {  
+            echo "From a new method in " . __CLASS__ . ".<br />";  
+        }  
     }  
-	
-}  
-$obj = new MyClass;  
-$obj2 = new Myclass;
-echo $obj->getProperty(); // Output the property
-echo $obj2->getProperty(); // Output the property
-$obj->setProperty('whatthe'); //Set the property
-$obj2->setProperty('wow repetition'); //Set the property
-echo $obj->getProperty();
-echo $obj2;
-
-echo "Objects are not destroyed until the end of the file or until you call unset()<br>";
-unset($obj);
-echo "obj destroyed by unsetting<br>";
-echo "then obj2 destroyed by letting the file reach the end<br>";
-
-?>  
+    // Create a new object  
+    $newobj = new MyOtherClass;
+	$newobj2=new myOtherClass;
+    // Output the object as a string  
+    echo $newobj->newMethod();  
+	echo $newobj2->getProperty();
+    // Use a method from the parent class  
+    echo $newobj->getProperty();  
+?> 
