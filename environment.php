7835ef972c5adcc7144784ebc96d70b73d1c6cd6
Fletcher <fboost@gmail.com>
2012-10-17 12:48:04 -0400

    ignore test

--- environment.php
+++ environment.php
@@ -7,7 +7,7 @@ Your local setup and your live setup should both have their own versions of this
 This file should not be tracked in your Git repo.
 -------------------------------------------------------------------------------------------------*/
 
-# We're on the local environment so toggle IN_PRODUCTION offf
+# We're on the local environment so toggle IN_PRODUCTION off
 define('IN_PRODUCTION', FALSE);
 
 # Always display errors on local environment
