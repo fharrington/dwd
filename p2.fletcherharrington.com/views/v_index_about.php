c80f62ec7681eddfc190f915276b9294bf574ea3
Fletcher <fboost@gmail.com>
2012-11-01 21:20:19 -0400

    added about view

---
+++ p2.fletcherharrington.com/views/v_index_about.php
@@ -0,0 +1,4 @@
+<?=$content?>
+<h1>About Saysomething</h1>
+<br><br>
+Say something was the brilliant idea of some guys who visited twitter once and decided to build a nearly identical site with less funcitonality.
