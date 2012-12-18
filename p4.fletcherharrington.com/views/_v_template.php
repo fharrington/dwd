06bcfc7fd7645257699d57d8fb0d629b475caa6d
Fletcher <fboost@gmail.com>
2012-12-18 15:08:30 -0500

    more tiles

--- p4.fletcherharrington.com/views/_v_template.php
+++ p4.fletcherharrington.com/views/_v_template.php
@@ -6,41 +6,16 @@
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/js/metalmixup.js"></script>
+<script type="text/javascript" src="/js/metalmixupsb.js"></script>
 <link rel="stylesheet" type="text/css" href="/css/users.css">
 
 
-<!--<script type="text/javascript">
-$(document).ready(function() { // start doc ready; do not delete this!
-
-
-tile_test.aud1.set_tile;
-
-
-}); // end doc ready; do not delete this!
-</script>
--->
 
 </head>
 <body>
 <div id = "wrap">
 	<div id = "page">
 	
-	<?
-
-	?>
-
-	
-	<? $audiofiles = Array("sound1", "sound2", "sound3"); ?>
-
-
-	
-	<? /*foreach ($audiofiles as $file)
-	echo "<audio preload title="{$file}">
-	<source src="audio/c1.wav"></source>
-	Browser!supportsound
-	</audio>" */
-	?>
 	
 	<audio preload="auto" title="sound1" id="audio1">
 	<source src="audio/sound1.wav"></source>
@@ -85,10 +60,38 @@ tile_test.aud1.set_tile;
 	Browser!supportsound
 	</audio>
 	
+	<audio preload="auto" title="sound7" id="audio7">
+	<source src="audio/sound7.wav"></source>
+	<source src="audio/sound7.ogg"></source>
+	<source src="audio/sound7.mp3"></source>
+	Browser!supportsound
+	</audio>
+	
+	<audio preload="auto" title="sound8" id="audio8">
+	<source src="audio/sound8.wav"></source>
+	<source src="audio/sound8.ogg"></source>
+	<source src="audio/sound8.mp3"></source>
+	Browser!supportsound
+	</audio>
+
+	<audio preload="auto" title="sound9" id="audio9">
+	<source src="audio/sound9.wav"></source>
+	<source src="audio/sound9.ogg"></source>
+	<source src="audio/sound9.mp3"></source>
+	Browser!supportsound
+	</audio>
+
+	<audio preload="auto" title="sound10" id="audio10">
+	<source src="audio/sound10.wav"></source>
+	<source src="audio/sound10.ogg"></source>
+	<source src="audio/sound10.mp3"></source>
+	Browser!supportsound
+	</audio>
+	
 	<h1>Metal Mix Up</h1>
 	<div class = "content">
 
-	<div id = "playbutton">++++</div>
+	<div id = "playbutton">[---]</div>
 	
 	<div id = "player"></div>
 		
