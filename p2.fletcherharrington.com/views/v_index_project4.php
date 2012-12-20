5a2fe8f91ea949ec7dd295f6774ee57b614c7f5b
fharrington <fboost@gmail.com>
2012-12-20 00:02:34 -0500

    ...

--- p2.fletcherharrington.com/views/v_index_project4.php
+++ p2.fletcherharrington.com/views/v_index_project4.php
@@ -1,5 +1,5 @@
 <div id='title'><h2>P4 proposal</h2></div>
 
 <div class='content'>
-For P4 i'd like to build on my 3rd project and create an audio player that plays sounds linked to tiles that the user arranges in order to create their own "song".
+For P4 i'd like to build on my <strike>3rd project and create an audio player that plays sounds linked to tiles that the user arranges in order to create their own "song"</strike> p2 project and create an audio sharing website for musicians to upload their new music and post it in a microblog format.  Users will be able to embed songs in posts and view their uploaded files in their profile.
 </div>
